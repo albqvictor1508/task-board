a3892592ff031dfb0445469364a37f4323961eda
albqvictor1508 <albq.victor@gmail.com>
2025-04-26 16:59:22 -0300

    user entity

--- src/main/java/me/dio/task_board/entity/User.java
+++ src/main/java/me/dio/task_board/entity/User.java
@@ -1,14 +1,28 @@
 package me.dio.task_board.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
 
 @Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 50, nullable = false)
     private String name;
-    @Column(length = 100, nullable = false, unique = true)
+    @Email
+    @Column(unique = true)
     private String email;
+    @Column(nullable = false, length = 150)
+    private String password;
 }
