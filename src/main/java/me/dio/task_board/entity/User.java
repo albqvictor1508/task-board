23c4c1a1bd4daaaa7a7fa0fb4dda6fdf95fb82e2
albqvictor1508 <albq.victor@gmail.com>
2025-04-26 10:34:29 -0300

    wip: creating user entity

--- src/main/java/me/dio/task_board/entity/User.java
+++ src/main/java/me/dio/task_board/entity/User.java
@@ -1,6 +1,14 @@
 package me.dio.task_board.entity;
 
-public class User {
-    private Long id;
+import jakarta.persistence.*;
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
 }
