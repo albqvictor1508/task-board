324b1eaa839cb3f6dcb57eadf17e4b6dd552ab97
albqvictor1508 <albq.victor@gmail.com>
2025-04-27 12:26:02 -0300

    chore: add getters and setter to task entity

--- src/main/java/me/dio/task_board/entity/Task.java
+++ src/main/java/me/dio/task_board/entity/Task.java
@@ -15,4 +15,46 @@ public class Task {
     @ManyToOne
     @JoinColumn(name = "board_id")
     private Board board;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, Board board) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.board = board;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 }
