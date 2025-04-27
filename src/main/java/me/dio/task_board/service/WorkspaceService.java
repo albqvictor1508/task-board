b85874168d4e22d67a25da2c15849c2a7093f521
albqvictor1508 <albq.victor@gmail.com>
2025-04-27 10:57:44 -0300

    chore: add new member method

--- src/main/java/me/dio/task_board/service/WorkspaceService.java
+++ src/main/java/me/dio/task_board/service/WorkspaceService.java
@@ -1,6 +1,8 @@
 package me.dio.task_board.service;
 
+import me.dio.task_board.entity.User;
 import me.dio.task_board.entity.Workspace;
+import me.dio.task_board.repository.UserRepository;
 import me.dio.task_board.repository.WorkspaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.List;
 public class WorkspaceService {
     @Autowired
     private WorkspaceRepository workspaceRepo;
+    @Autowired
+    private UserRepository userRepo;
 
     public void addNewWorkspace(Workspace ws) {
         workspaceRepo.save(ws);
@@ -26,5 +30,16 @@ public class WorkspaceService {
 
     public Workspace findById(Long id) {
         return workspaceRepo.findById(id).get();
+    }
+
+    public void addNewMember(Long workspaceId, Long userId) {
+        Workspace ws = workspaceRepo.findById(workspaceId).get();
+        User u = userRepo.findById(userId).get();
+
+        if(ws.getMembers().contains(u)) {
+            throw new RuntimeException("user is already in workspace");
+        }
+            ws.getMembers().add(u);
+            workspaceRepo.save(ws);
     }
 }
