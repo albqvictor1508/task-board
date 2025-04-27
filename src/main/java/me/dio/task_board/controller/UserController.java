cf3dbd7bb3139836d1a1270ff5fdda1e29bba8c1
albqvictor1508 <albq.victor@gmail.com>
2025-04-27 09:23:59 -0300

    chore: update user DTO created

--- src/main/java/me/dio/task_board/controller/UserController.java
+++ src/main/java/me/dio/task_board/controller/UserController.java
@@ -1,15 +1,14 @@
 package me.dio.task_board.controller;
 
+import me.dio.task_board.dto.UserDTO;
 import me.dio.task_board.entity.User;
 import me.dio.task_board.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -19,7 +18,6 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> save(@RequestBody User u) {
-        if(service.existsById(u.getId())) return ResponseEntity.status(401).body("this user already exists");
         service.save(u);
         Object success = new HashMap<>().put("success", true);
         return ResponseEntity.status(201).body(success);
@@ -30,10 +28,8 @@ public class UserController {
         return service.findAll();
     }
 
-
-
     @PutMapping
-    public User updateUser(@RequestBody User u) {
+    public User updateUser(@RequestBody UserDTO u) {
         return service.update(u);
     }
 
--- src/main/java/me/dio/task_board/service/UserService.java
+++ src/main/java/me/dio/task_board/service/UserService.java
@@ -1,5 +1,6 @@
 package me.dio.task_board.service;
 
+import me.dio.task_board.dto.UserDTO;
 import me.dio.task_board.entity.User;
 import me.dio.task_board.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class UserService {
         return userRepo.findById(id).get();
     }
 
-    public User update(User newUserData) {
+    public User update(UserDTO newUserData) {
         User u = userRepo.findById(newUserData.getId()).get();
         u.setName(newUserData.getName());
         u.setEmail(newUserData.getEmail());
