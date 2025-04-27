8345e2f4be20e1590a33164bc2d62f31ae946d63
albqvictor1508 <albq.victor@gmail.com>
2025-04-27 12:21:22 -0300

    chore: add constructor, getter and setter

--- src/main/java/me/dio/task_board/entity/Board.java
+++ src/main/java/me/dio/task_board/entity/Board.java
@@ -10,6 +10,49 @@ public class Board {
     private Long id;
     @Column(length = 100, nullable = false)
     private String title;
+
+    public Board(Long id, String title, String description, Workspace workspace) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.workspace = workspace;
+    }
+
+    public Board() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Workspace getWorkspace() {
+        return workspace;
+    }
+
+    public void setWorkspace(Workspace workspace) {
+        this.workspace = workspace;
+    }
+
     //posso fazer tags
     @Column(nullable = true)
     private String description;
